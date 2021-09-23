5fd7e2e3d87e3e1b0b81a4283f5e3d5dfc8f635c
Chet Corcos <ccorcos@gmail.com>
2021-09-22 20:10:23 -0700

    I think this works.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,35 +10,41 @@ const chord = [
 	5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
 ]
 
+const padding = 0
+const numberOfNotes = 6 * 12 + 1
+const maxBallRadius = 18
+const minBallRadius = 3
+
+// Compute the radius of the ith ball.
+function radius(i) {
+	return Math.max(maxBallRadius - i / 2.5, minBallRadius)
+}
+
+// Distance to the first note.
+const d0 = ((maxBallRadius * 24) / 2 / Math.PI) * 0.8
+
+// Distance between d0 and d12
+const g0 = radius(0) + radius(12) + padding
+
+// Tell Sean: error in my previous code is that g0 is the only g that matters.
+
+function distance(i) {
+	const offset = i % 12
+	const octave = Math.floor(i / 12)
+
+	// Distance to the inner most circle for this offset.
+	const inner = d0 + (offset * g0) / 12
+
+	let dist = inner
+	for (let j = 0; j < octave; j++) {
+		dist += radius(j * 12 + offset) + radius((j + 1) * 12 + offset) + padding
+	}
+
+	return dist
+}
+
 function App() {
-	const pad = 2
-	const notes = 6 * 12 + 1
-	const maxBall = 18
-	const minBall = 3
-	const minDist = ((maxBall * 24) / 2 / Math.PI) * 0.8
-
-	function radius(i) {
-		// return Math.max(maxBall - i/3, minBall)
-		return Math.max(maxBall - i / 2.5, minBall)
-	}
-
-	function innerDist(i) {
-		// 0 < i < 12
-		return minDist + (radius(0) + radius(12) + pad) / 12
-	}
-
-	function distance(i) {
-		if (i === 0) return minDist + radius(0)
-
-		if (i < 12) {
-			const doctave = radius(i - 1) + radius(i + 11)
-			return distance(i - 1) + doctave / 12
-		}
-
-		return distance(i - 12) + radius(i - 12) + radius(i) + pad
-	}
-
-	const circles = range(0, notes).map((i) => {
+	const circles = range(0, numberOfNotes).map((i) => {
 		const ballRadius = radius(i)
 		const dist = distance(i)
 
@@ -56,8 +62,7 @@ function App() {
 					height: 2 * ballRadius,
 					width: 2 * ballRadius,
 					background: chord.includes(i) ? "#FFBCBC" : "#BCBCBC",
-					left: center + x,
-					top: center + y,
+					transform: `translate(${center + x}px, ${center + y}px)`,
 					borderRadius: "100%",
 				}}
 			></div>
